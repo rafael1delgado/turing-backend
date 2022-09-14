f6d82595662f055359c2831e91dd925b1bd0af81
sebastiaannavas <sebastiannavas06@gmail.com>
2022-09-14 00:23:43 -0400

    fix: enable cors policy

--- validation/user.js
+++ validation/user.js
@@ -1,10 +1,15 @@
 const yup = require('yup');
 
 let userSchema = yup.object({
-    name: yup.string().min(2).max(255).required(),
-    last_name: yup.string().min(2).max(255).required(),
+    // name: yup.string().min(2).max(255).required(),
+    // last_name: yup.string().min(2).max(255).required(),
+    // email: yup.string().email().min(3).max(255).required(),
+    // password: yup.string().min(8).max(20).required()
+    fname: yup.string().min(2).max(255).required(),
+    lname: yup.string().min(2).max(255).required(),
+    alias: yup.string().min(2).max(255).required(),
     email: yup.string().email().min(3).max(255).required(),
-    password: yup.string().min(8).max(20).required()
+    psw: yup.string().min(8).max(20).required()
 });
 
 module.exports = {
