224bc36f2dedbf8ff0ebf6252604edbfd900c5a1
rafael1delgado <delgadorafael2011@gmail.com>
2022-09-17 23:05:00 -0400

    feat: update message & token

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,27 +1,25 @@
 const jwt = require("jsonwebtoken");
-const {
-  SECRET: { SECRET_TOKEN },
-} = require("./utils");
 const { client } = require("./conect-mongodb");
+require('dotenv').config();
 
 async function verifyJwt(auth) {
   try {
     const TOKEN = auth[0].split(" ")[1];
-    const decode = jwt.verify(TOKEN, SECRET_TOKEN);
+    const decode = jwt.verify(TOKEN, process.env.SECRET_TOKEN);
     await client.connect();
     const collectionUsers = client.db().collection("users");
 
     const user = await collectionUsers.findOne({ email: decode.email });
     if (user) {
       if (user.iat > decode.iat) {
-        return { error: "Token expirado", user };
+        return { error: "Token expirado o inválido", user };
       }
       return { error: false, user };
     }
     return { error: "Error de autenticación", user };
   } catch (error) {
     console.log(error)
-    return { error: "Token expirado" };
+    return { error: "Token expirado o inválido" };
   }
 }
 
