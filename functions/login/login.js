cff23a33e5b3ec5b2536ffb099ddff3a28089d3d
sebastiaannavas <sebastiannavas06@gmail.com>
2022-09-21 07:48:51 -0400

    fix: email tls and login integration

--- functions/login/login.js
+++ functions/login/login.js
@@ -7,46 +7,56 @@ const { output } = require("../../utils/utils");
 require("dotenv").config();
 
 const handler = async (event) => {
-  try {
-    let { httpMethod: method } = event;
-    const data = event.body
-    const { email, psw } = data;
 
-    await client.connect();
-    const collectionUsers = client.db().collection("users");
-    const users = await collectionUsers.find({ email: email }).toArray();
+  try {
+
+    let { httpMethod: method } = event;
+    
+    if (method === "OPTIONS") {
+      return output("success", 200);
+    }
 
     if (method == "POST") {
-        if (users.length > 0) {
-        const validPassword = await bcrypt.compare(psw, users[0].psw);
 
-        if (validPassword) {
-          const token = await jwt.sign({ email: email }, process.env.SECRET_TOKEN, {
-            expiresIn: "12h",
-          });
-          const iat = Math.round(Date.now()/1000);
-          collectionUsers.updateOne(
-            { email },
-            { $set: { iat } },
-            { $upsert: true }
-          );
+        let data = event.body
+        let { email, psw } = data;
+        email = email.toLowerCase();
+    
+        await client.connect();
+        const collectionUsers = client.db().collection("users");
+        const users = await collectionUsers.find({ email: email }).toArray();
 
-          return output({ msg: "Bienvenido a la Wallet Turing.", token }, 200);
-        } else {
+        if (users && users.length > 0) {
+          const validPassword = await bcrypt.compare(psw, users[0].psw);
+
+          if (validPassword) {
+            const token = await jwt.sign({ email: email }, process.env.SECRET_TOKEN, {
+              expiresIn: "12h",
+            });
+            const iat = Math.round(Date.now()/1000);
+            collectionUsers.updateOne(
+              { email },
+              { $set: { iat } },
+              { $upsert: true }
+            );
+
+            return output({ msg: "Bienvenido a la Wallet Turing.", token }, 200);
+          } else {
+            return output(
+              { error: "Disculpe, el usuario y/o la contraseña es errada" },
+              400
+            );
+          }
+        } 
+        else {
           return output(
-            { error: "Disculpe, el usuario y/o la contraseña es errada" },
+            { error: "El usuario no coincide con nuestros registros." },
             400
           );
         }
-      } else {
-        return output(
-          { error: "El usuario no coincide con nuestros registros." },
-          400
-        );
-      }
     }
   } catch (error) {
-    return output({ error: 'La contraseña es invalida.'}, 400);
+    return output({ error: 'Se produjo un problema.'}, 500);
   }
 };
 
