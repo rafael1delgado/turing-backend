1473b64a6d8d818b6a72b5722514072b801cd08e
rafael1delgado <delgadorafael2011@gmail.com>
2022-09-20 23:56:37 -0400

    feat: update response & rename data

--- functions/login/login.js
+++ functions/login/login.js
@@ -10,7 +10,7 @@ const handler = async (event) => {
   try {
     let { httpMethod: method } = event;
     const data = event.body
-    const { email, password } = data;
+    const { email, psw } = data;
 
     await client.connect();
     const collectionUsers = client.db().collection("users");
@@ -18,7 +18,7 @@ const handler = async (event) => {
 
     if (method == "POST") {
         if (users.length > 0) {
-        const validPassword = await bcrypt.compare(password, users[0].psw);
+        const validPassword = await bcrypt.compare(psw, users[0].psw);
 
         if (validPassword) {
           const token = await jwt.sign({ email: email }, process.env.SECRET_TOKEN, {
@@ -34,19 +34,19 @@ const handler = async (event) => {
           return output({ msg: "Bienvenido a la Wallet Turing.", token }, 200);
         } else {
           return output(
-            { msg: "Disculpe, el usuario y/o la contraseña es errada" },
+            { error: "Disculpe, el usuario y/o la contraseña es errada" },
             400
           );
         }
       } else {
         return output(
-          { msg: "El usuario no coincide con nuestros registros." },
+          { error: "El usuario no coincide con nuestros registros." },
           400
         );
       }
     }
   } catch (error) {
-    return output({ error: error }, 400);
+    return output({ error: 'La contraseña es invalida.'}, 400);
   }
 };
 
