5ace5e9aa32acf52357991f17dedd68da8af5d88
pedro <Pecarvajal246@gmail.com>
2022-09-24 01:21:31 -0400

    fix: remove client.close()

--- functions/getEmailVerification/getEmailVerification.js
+++ functions/getEmailVerification/getEmailVerification.js
@@ -14,13 +14,14 @@ const handler = async (event) => {
       const decode = jwt.verify(emailToken, process.env.SECRET_TOKEN);
       await client.connect();
       const collectionUsers = client.db().collection("users");
-      collectionUsers.updateOne({ email: decode.email }, { $set: { verified: true } });
-      return output({msg: "Correo verificado exitosamente"}, 200)
+      collectionUsers.updateOne(
+        { email: decode.email },
+        { $set: { verified: true } }
+      );
+      return output({ msg: "Correo verificado exitosamente" }, 200);
     } catch (error) {
       console.log(error);
       return output({ error: "Token expirado o inválido" }, 500);
-    } finally {
-        await client.close();
     }
   }
 };
