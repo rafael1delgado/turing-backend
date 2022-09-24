7c6dac1ead6717fb15214c1d5e2f307f42fcaaef
pedro <Pecarvajal246@gmail.com>
2022-09-24 01:33:03 -0400

    fix: await collection

--- functions/getEmailVerification/getEmailVerification.js
+++ functions/getEmailVerification/getEmailVerification.js
@@ -14,14 +14,12 @@ const handler = async (event) => {
       const decode = jwt.verify(emailToken, process.env.SECRET_TOKEN);
       await client.connect();
       const collectionUsers = client.db().collection("users");
-      collectionUsers.updateOne(
-        { email: decode.email },
-        { $set: { verified: true } }
-      );
-      return output({ msg: "Correo verificado exitosamente" }, 200);
+      await collectionUsers.updateOne({ email: decode.email }, { $set: { verified: true } });
+      return output({msg: "Correo verificado exitosamente"}, 200)
     } catch (error) {
-      console.log(error);
       return output({ error: "Token expirado o inválido" }, 500);
+    } finally {
+        await client.close();
     }
   }
 };
