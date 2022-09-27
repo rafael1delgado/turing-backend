9294bce3836bfb641dcb515ef6b355e8ccd43e69
rafael1delgado <delgadorafael2011@gmail.com>
2022-09-27 11:43:50 -0400

    feat: update verify

--- utils/verify.js
+++ utils/verify.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 let jwt = require("jsonwebtoken");
 
-// let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Iml2YW4xQGdtYWlsLmNvbSIsImlhdCI6MTY2MzcyOTQ1NiwiZXhwIjoxNjYzNzcyNjU2fQ.1Kj1--RkmvWez9wTVOEdG7EsOQegehDINHSSg2hP3R0"
 let tokenOld = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJhZmFlbEBnbWFpbC5jb20iLCJpYXQiOjE2NjM2MzE1MTQsImV4cCI6MTY2MzY3NDcxNH0.qLr16nzlvx4KmqJ3ubGN-DcWJJYU_MNRBMcbcrVhN3g"
-// console.log(process.env.SECRET_TOKEN)
 let decoded = jwt.verify(tokenOld, process.env.SECRET_TOKEN);
 console.log(decoded);
 
@@ -12,15 +10,15 @@ var exp = new Date(decoded.exp * 1000); // expira el
 var now = new Date(); // fecha actual
 
 console.log(iat <= now <= exp);
-// console.log(iat);
-// console.log(exp);
-// console.log(now);
+console.log(iat);
+console.log(exp);
+console.log(now);
 
-// console.log(iat.toLocaleString());
-// console.log(exp.toLocaleString());
-// console.log(now.toLocaleString());
+console.log(iat.toLocaleString());
+console.log(exp.toLocaleString());
+console.log(now.toLocaleString());
 
 
-// const token = event.multiValueHeaders.Authorization[0].split(" ")[1]
-// let decoded = await jwt.verify(token, TU_TOKEN);
-// return output({msg: 'Bien', decoded: decoded}, 200);
+const token = event.multiValueHeaders.Authorization[0].split(" ")[1]
+decoded = await jwt.verify(token, TU_TOKEN);
+return output({msg: 'Bien', decoded: decoded}, 200);
