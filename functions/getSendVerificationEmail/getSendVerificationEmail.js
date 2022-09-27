bda08097c8a8213b588d10401504fcb8b4928470
pedro <Pecarvajal246@gmail.com>
2022-09-27 01:20:33 -0400

    feat: add email template

--- functions/getSendVerificationEmail/getSendVerificationEmail.js
+++ functions/getSendVerificationEmail/getSendVerificationEmail.js
@@ -4,7 +4,7 @@ let { httpHeaderNormalizer } = require("middy/middlewares");
 require("dotenv").config();
 const { output } = require("../../utils/utils");
 const { sendEmail } = require("../../utils/email");
-const { mjmlApi } = require("../../utils/mjml");
+const { getHtmlWithButton } = require("../../utils/mjml");
 
 const handler = async (event) => {
   let { httpMethod: method } = event;
@@ -16,34 +16,12 @@ const handler = async (event) => {
       const emailToken = jwt.sign({ email: email }, process.env.SECRET_TOKEN, {
         expiresIn: "1d",
       });
-      // url en el frontend
       const url = `${process.env.FRONTEND_HOST}/verification?emailToken=${emailToken}`;
-
-      // url para prueba en el backend
-      // const url = `http://localhost:8888/getEmailVerification?emailToken=${emailToken}`;
-
-      // const text = `Por favor verifica tu email haciendo click en el siguiente <a href="${url}">link</a>`;
-      const mjml = `<mjml>
-  <mj-body>
-    <mj-section>
-      <mj-column>
-
-        <mj-image width="100px" src="https://i.ibb.co/JszvgN5/photo-2022-08-19-05-54-07.jpg"></mj-image>
-
-        <mj-divider border-color="#512999"></mj-divider>
-
-        <mj-text font-size="20px" color="#8C53F0" font-family="helvetica">Bienvenido a Turing Exchange. Por favor verifica tu email presionando el siguiente bot\u00f3n.</mj-text>
-<mj-button background-color="#8C53F0" border-radius="15px"
-                 href="${url}">
-        Verificar
-      </mj-button>
-
-      </mj-column>
-    </mj-section>
-  </mj-body>
-</mjml>`;
-      const { data } = await mjmlApi.post("/render", { mjml });
-      sendEmail(email, "Verificación de email", data.html);
+      const text =
+        "Bienvenido a Turing Exchange. Por favor verifica tu email presionando el siguiente bot\u00f3n.";
+      const buttonLabel = "Verificar email";
+      const html = await getHtmlWithButton(text, buttonLabel, url);
+      sendEmail(email, "Verificación de email", html);
       return output({ msg: "Correo enviado exitosamente" }, 200);
     } catch (error) {
       return output({ error }, 500);
--- utils/mjml.js
+++ utils/mjml.js
@@ -1,13 +1,15 @@
 const axios = require("axios");
+require('dotenv').config();
 
 const mjmlApi = axios.create({
   baseURL: "https://api.mjml.io/v1",
   auth: {
-    username: "6fbe23f2-2e9c-4dc8-9af1-2c7dca86c5e5",
-    password: "d8d8b2e2-f8ec-4f07-ab93-276384bfbc07",
+    username: process.env.MJML_ID,
+    password: process.env.MJML_SECRET,
   },
 });
-const mjml = `<mjml>
+async function getHtmlWithButton(text, buttonLabel, link) {
+  const mjml = `<mjml>
   <mj-body>
     <mj-section>
       <mj-column>
@@ -16,19 +18,37 @@ const mjml = `<mjml>
 
         <mj-divider border-color="#512999"></mj-divider>
 
-        <mj-text font-size="20px" color="#8C53F0" font-family="helvetica">Bienvenido a Turing Exchange. Por favor verifica tu email presionando el siguiente bot\u00f3n.</mj-text>
+        <mj-text font-size="20px" color="#8C53F0" font-family="helvetica">${text}</mj-text>
 <mj-button background-color="#8C53F0" border-radius="15px"
-                 href="#">
-        Verificar
+                 href="${link}">
+      ${buttonLabel}
       </mj-button>
 
       </mj-column>
     </mj-section>
   </mj-body>
 </mjml>`;
-async function getHtml() {
   const { data } = await mjmlApi.post("/render", { mjml });
+  return data.html;
 }
- getHtml()
+async function getHtml(text) {
+  const mjml = `<mjml>
+  <mj-body>
+    <mj-section>
+      <mj-column>
 
-module.exports = { mjmlApi };
+        <mj-image width="100px" src="https://i.ibb.co/JszvgN5/photo-2022-08-19-05-54-07.jpg"></mj-image>
+
+        <mj-divider border-color="#512999"></mj-divider>
+
+        <mj-text font-size="20px" color="#8C53F0" font-family="helvetica">${text}</mj-text>
+
+      </mj-column>
+    </mj-section>
+  </mj-body>
+</mjml>`;
+  const { data } = await mjmlApi.post("/render", { mjml });
+  return data.html;
+}
+
+module.exports = {  getHtmlWithButton };
